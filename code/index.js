2c9d4b1c6a903d95ec725ec35301df3a9142b720
GiovanniSia <76817363+GiovanniSia@users.noreply.github.com>
2021-03-21 14:29:28 -0300

    Agregar tarea tiene datetime
    Las tareas tienen fecha y hora. Se puede apretar Enter en datetime para presionar el botón "Agregar tarea".

--- code/index.js
+++ code/index.js
@@ -1,5 +1,5 @@
-//A "numero" lo sacamos calculando las cantidad de divs "tarea" tenemos.
-function crearCheckTarea(numero, texto) {
+
+function crearCheckTarea(numero, texto, fechaHora) {
 
     const $nodo = document.querySelector("#tareas");
 
@@ -13,19 +13,39 @@ function crearCheckTarea(numero, texto) {
     const nodoLabel = document.createElement("label");
     nodoLabel.textContent = texto;
 
+    const nodoFechaHora = document.createElement("input");
+    nodoFechaHora.type = "datetime-local";
+    nodoFechaHora.disabled = true;
+    nodoFechaHora.value = fechaHora;
+
     div.appendChild(nodoInput);
     div.appendChild(nodoLabel);
+    div.appendChild(nodoFechaHora);
     $nodo.appendChild(div);
+
 }
 
-function limpiarInputTarea(){
-    document.querySelector("#texto-tarea").value="";
+function limpiarInputsTarea() {
+    document.querySelector("#texto-tarea").value = "";
+    document.querySelector("#fecha-hora-tarea").value = "";
+}
+
+function cantidadDeTareas() {
+    const listaTareas = document.querySelectorAll(".tarea");
+    let contador = 0;
+    for (let i = 0; i < listaTareas.length; i++) {
+        contador++;
+    }
+    return contador;
 }
 
 function agregarTarea() {
-    const textoTarea = document.querySelector("#texto-tarea").value;
-    crearCheckTarea(1, textoTarea);
-    limpiarInputTarea();
+    const texto = document.querySelector("#texto-tarea").value;
+    const numero = cantidadDeTareas();
+    const fechaHora = document.querySelector("#fecha-hora-tarea").value;
+    crearCheckTarea(numero, texto, fechaHora);
+    limpiarInputsTarea();
+
 }
 
 function limpiarTareas() {
@@ -40,13 +60,23 @@ $botonAgregarTarea.onclick = agregarTarea;
 
 //En vez de usar el boton "Agregar tarea" puedo usar la tecla "Enter"
 const input = document.querySelector("#texto-tarea");
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   document.querySelector("#boton-agregar-tarea").click();
-  }
+input.addEventListener("keyup", function (event) {
+    enter(event);
 });
 
+//En vez de usar el boton "Agregar tarea" puedo usar la tecla "Enter"
+const input2 = document.querySelector("#fecha-hora-tarea");
+input2.addEventListener("keyup", function (event) {
+    enter(event);
+});
+
+function enter(event){
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector("#boton-agregar-tarea").click();
+    }
+}
 const $botonLimpiarTareas = document.querySelector("#boton-limpiar-tareas");
 $botonLimpiarTareas.onclick = limpiarTareas;
+
 
